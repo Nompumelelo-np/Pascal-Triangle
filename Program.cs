ba09b9ae7d6cefa34e3a42a14fe664d2de161f1a
Nompumelelo Hlongwane <61636340+Nompumelelo-np@users.noreply.github.com>
2024-03-08 19:13:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+   Console.WriteLine(" "+" "+ " " + " " + 1);
+   Console.WriteLine(" "+" " +  " " + 1 + " " + 1);
+   Console.WriteLine(" "+ " " + 1 + " " + 2 + " "+ 1);
+   Console.WriteLine(" " + 1 + " " + 3 + " " + 3 + " " + 1);
+   Console.WriteLine(1 + " " + 4 + " " + 6 + " " + 4 + " " + 1);  
+
